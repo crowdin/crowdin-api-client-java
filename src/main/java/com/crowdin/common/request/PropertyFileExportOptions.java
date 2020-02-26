f1765c976f5539cf169381dc92fc736dd239d9bc
fromBetelgeuse <dBarabash42@gmail.com>
2020-02-26 23:51:38 +0200

    Add field escapeSpecialCharacters to PropertyFileExportOptions

--- src/main/java/com/crowdin/common/request/PropertyFileExportOptions.java
+++ src/main/java/com/crowdin/common/request/PropertyFileExportOptions.java
@@ -3,6 +3,7 @@ package com.crowdin.common.request;
 public class PropertyFileExportOptions implements ExportOptions {
 
     private Integer escapeQuotes;
+    private Integer escapeSpecialCharacters;
     private String exportPattern;
 
     public Integer getEscapeQuotes() {
@@ -11,6 +12,14 @@ public class PropertyFileExportOptions implements ExportOptions {
 
     public void setEscapeQuotes(Integer escapeQuotes) {
         this.escapeQuotes = escapeQuotes;
+    }
+
+    public Integer getEscapeSpecialCharacters() {
+        return escapeSpecialCharacters;
+    }
+
+    public void setEscapeSpecialCharacters(Integer escapeSpecialCharacters) {
+        this.escapeSpecialCharacters = escapeSpecialCharacters;
     }
 
     public String getExportPattern() {
