dd896e5b1abdbdc1dd09e8ef9da26ed8b5ab63bb
Yevheniy Oliynyk <evgen41293@gmail.com>
2020-04-10 22:18:50 +0300

    added tests for project groups, source files and workflow APIs

--- src/main/java/com/crowdin/client/workflows/model/WorkflowStep.java
+++ src/main/java/com/crowdin/client/workflows/model/WorkflowStep.java
@@ -3,7 +3,6 @@ package com.crowdin.client.workflows.model;
 import lombok.Data;
 
 import java.util.List;
-import java.util.Map;
 
 @Data
 public class WorkflowStep {
@@ -12,10 +11,5 @@ public class WorkflowStep {
     private String title;
     private String type;
     private List<String> languages;
-    private Config config;
-
-    @Data
-    public static class Config {
-        private Map<String, List<Integer>> assignees;
-    }
+    private Object config;
 }
