07b412f052dbf699a8f8f9301f71964f7c5df956
Daniil Barabash <dbarabash42@gmail.com>
2020-09-03 15:14:59 +0300

    Version 1.2.9

--- src/main/java/com/crowdin/client/Sandbox.java
+++ src/main/java/com/crowdin/client/Sandbox.java
@@ -4,6 +4,7 @@ import com.crowdin.client.core.http.exceptions.HttpBadRequestException;
 import com.crowdin.client.core.http.exceptions.HttpException;
 import com.crowdin.client.core.model.Credentials;
 import com.crowdin.client.core.model.ResponseObject;
+import com.crowdin.client.projectsgroups.model.ProjectSettings;
 import com.crowdin.client.users.model.User;
 import lombok.var;
 
@@ -12,17 +13,33 @@ import java.io.FileNotFoundException;
 public class Sandbox {
 
     public static void main(String[] args) throws FileNotFoundException, InterruptedException {
-        if (args.length > 0) {
-            try {
-                Credentials credentials = new Credentials(args[0], "oliynyk");
-                var client = new Client(credentials);
-                ResponseObject<User> user = client.getUsersApi().getAuthenticatedUser();
-                System.out.println(user.getData());
-            } catch (HttpException e) {
-                System.out.println(e.getError());
-            } catch (HttpBadRequestException e) {
-                System.out.println(e.getErrors());
-            }
-        }
+
+        new Integer(null);
+
+        if (true) return;
+
+        Credentials credentials = new Credentials("1af38c02dd24a61fa721cc77e099ff9508f8e58b8417ba040b18507e5f5cbffe4ddcdcb84e5610c7", null);
+        Client client = new Client(credentials);
+
+        ProjectSettings project = (ProjectSettings) client.getProjectsGroupsApi()
+            .getProject(383090L)
+            .getData();
+
+        System.out.println("project = " + project);
+
+        if (true) return;
+
+//        if (args.length > 0) {
+//            try {
+//                Credentialzs credentials = new Credentials(args[0], "oliynyk");
+//                var client = new Client(credentials);
+//                ResponseObject<User> user = client.getUsersApi().getAuthenticatedUser();
+//                System.out.println(user.getData());
+//            } catch (HttpException e) {
+//                System.out.println(e.getError());
+//            } catch (HttpBadRequestException e) {
+//                System.out.println(e.getErrors());
+//            }
+//        }
     }
 }
