71050d7dbddae95d0f04f4c8712c461ad4f5ea12
Andrii Bodnar <andrii.bodnar@crowdin.com>
2023-09-01 11:14:17 +0300

    fix: exportQuotes deserialization (#184)

--- src/main/java/com/crowdin/client/sourcefiles/model/ExportQuotes.java
+++ src/main/java/com/crowdin/client/sourcefiles/model/ExportQuotes.java
@@ -1,15 +1,19 @@
 package com.crowdin.client.sourcefiles.model;
 
 import com.crowdin.client.core.model.EnumConverter;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
 
 public enum ExportQuotes implements EnumConverter<ExportQuotes> {
 
     SINGLE, DOUBLE;
 
+    @JsonCreator
     public static ExportQuotes from(String value) {
         return ExportQuotes.valueOf(value.toUpperCase());
     }
 
+    @JsonValue
     @Override
     public String to(ExportQuotes v) {
         return v.name().toLowerCase();
