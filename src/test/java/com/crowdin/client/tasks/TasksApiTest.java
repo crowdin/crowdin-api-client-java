4801006dd761d21fe0806adfdb06fdf7d122ff19
Akash Pambhar <71589813+akashpambhar@users.noreply.github.com>
2023-10-17 12:51:28 +0530

    test(tasks): add coverage for tasks/model (#203)

--- src/test/java/com/crowdin/client/tasks/TasksApiTest.java
+++ src/test/java/com/crowdin/client/tasks/TasksApiTest.java
@@ -12,6 +12,9 @@ import com.crowdin.client.tasks.model.CrowdinTaskCreateFormRequest;
 import com.crowdin.client.tasks.model.EnterpriseTaskCreateFormRequest;
 import com.crowdin.client.tasks.model.Status;
 import com.crowdin.client.tasks.model.Task;
+import com.crowdin.client.tasks.model.Assignee;
+import com.crowdin.client.tasks.model.AssignedTeam;
+import com.crowdin.client.tasks.model.Progress;
 import org.apache.http.client.methods.HttpDelete;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPatch;
@@ -53,12 +56,20 @@ public class TasksApiTest extends TestClient {
 
     @Test
     public void listTasksTest() {
+        Assignee assignee = new Assignee();
+        assignee.setId(1L);
+        assignee.setUsername("john_smith");
+        assignee.setFullName("john_smith");
+        assignee.setAvatarUrl("");
+        assignee.setWordsCount(5);
+        assignee.setWordsLeft(3);
         TimeZone.setDefault(TimeZone.getTimeZone("GMT"));
         ResponseList<Task> taskResponseList = this.getTasksApi().listTasks(projectId, null, null, null);
         assertEquals(taskResponseList.getData().size(), 1);
         assertEquals(taskResponseList.getData().get(0).getData().getId(), taskId);
         assertEquals(taskResponseList.getData().get(0).getData().getStatus(), status);
         assertEquals(new Date(119, Calendar.SEPTEMBER,27,7,0,14), taskResponseList.getData().get(0).getData().getDeadline());
+        assertEquals(taskResponseList.getData().get(0).getData().getAssignees().get(0), assignee);
     }
 
     @Test
@@ -72,6 +83,32 @@ public class TasksApiTest extends TestClient {
         ResponseObject<Task> taskResponseObject = this.getTasksApi().addTask(projectId, request);
         assertEquals(taskResponseObject.getData().getId(), taskId);
         assertEquals(taskResponseObject.getData().getStatus(), status);
+        assertEquals(taskResponseObject.getData().getProjectId(), projectId);
+        assertEquals(taskResponseObject.getData().getCreatorId(), 6);
+        assertEquals(taskResponseObject.getData().getType(), 1);
+        assertEquals(taskResponseObject.getData().getVendor(), "gengo");
+        assertEquals(taskResponseObject.getData().getTitle(), "French");
+        assertEquals(taskResponseObject.getData().getFileIds().get(0), 1);
+        assertEquals(taskResponseObject.getData().getSourceLanguageId(), "en");
+        assertEquals(taskResponseObject.getData().getTargetLanguageId(), "fr");
+        assertEquals(taskResponseObject.getData().getDescription(), "Proofread all French strings");
+        assertEquals(taskResponseObject.getData().getHash(), "dac37aff364d83899128e68afe0de4994");
+        assertEquals(taskResponseObject.getData().getTranslationUrl(), "/proofread/9092638ac9f2a2d1b5571d08edc53763/all/en-fr/10?task=dac37aff364d83899128e68afe0de4994");
+        assertEquals(taskResponseObject.getData().getWordsCount(), 24);
+        assertEquals(taskResponseObject.getData().getFilesCount(), 2);
+        assertEquals(taskResponseObject.getData().getCommentsCount(), 0);
+        assertEquals(taskResponseObject.getData().getTimeRange(), "string");
+        assertEquals(taskResponseObject.getData().getWorkflowStepId(), 10);
+        assertEquals(taskResponseObject.getData().getBuyUrl(), "https://www.paypal.com/cgi-bin/webscr?cmd=...");
+        AssignedTeam assignedTeam = new AssignedTeam();
+        assignedTeam.setId(1L);
+        assignedTeam.setWordsCount(5);
+        assertEquals(taskResponseObject.getData().getAssignedTeams().get(0), assignedTeam);
+        Progress progress = new Progress();
+        progress.setTotal(24);
+        progress.setDone(15);
+        progress.setPercent(62);
+        assertEquals(taskResponseObject.getData().getProgress(), progress);
     }
 
     @Test
