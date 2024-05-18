f28728100d778af0a873fce80984a9c4ae7083bf
Yevheniy Oliynyk <evgen41293@gmail.com>
2024-05-18 08:39:42 +0200

    feat: pre-translate ai options

--- src/main/java/com/crowdin/client/translations/model/ApplyPreTranslationRequest.java
+++ src/main/java/com/crowdin/client/translations/model/ApplyPreTranslationRequest.java
@@ -3,6 +3,7 @@ package com.crowdin.client.translations.model;
 import lombok.Data;
 
 import java.util.List;
+import java.util.Map;
 
 @Data
 public class ApplyPreTranslationRequest {
@@ -11,10 +12,14 @@ public class ApplyPreTranslationRequest {
     private List<Long> fileIds;
     private Method method;
     private Long engineId;
+    private Long aiPromptId;
     private AutoApproveOption autoApproveOption;
     private Boolean duplicateTranslations;
+    private Boolean skipApprovedTranslations;
     private Boolean translateUntranslatedOnly;
     private Boolean translateWithPerfectMatchOnly;
+    private Map<String, List<String>> fallbackLanguages;
     private List<Long> labelIds;
     private List<Long> excludeLabelIds;
+
 }
