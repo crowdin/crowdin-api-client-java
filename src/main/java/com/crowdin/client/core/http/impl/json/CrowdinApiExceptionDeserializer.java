829fb923ad1324324ecfbab62e3f4ea9afce23b9
maksim.murujev <maksim.murujev@wearemp.com>
2024-09-06 11:14:31 +0300

    Fix for bulk error response

--- src/main/java/com/crowdin/client/core/http/impl/json/CrowdinApiExceptionDeserializer.java
+++ src/main/java/com/crowdin/client/core/http/impl/json/CrowdinApiExceptionDeserializer.java
@@ -2,6 +2,7 @@ package com.crowdin.client.core.http.impl.json;
 
 import com.crowdin.client.core.http.exceptions.CrowdinApiException;
 import com.crowdin.client.core.http.exceptions.HttpBadRequestException;
+import com.crowdin.client.core.http.exceptions.HttpBatchBadRequestException;
 import com.crowdin.client.core.http.exceptions.HttpException;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.TreeNode;
@@ -10,9 +11,6 @@ import com.fasterxml.jackson.databind.JsonDeserializer;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.StreamSupport;
 
 public class CrowdinApiExceptionDeserializer extends JsonDeserializer<CrowdinApiException> {
 
@@ -25,14 +23,18 @@ public class CrowdinApiExceptionDeserializer extends JsonDeserializer<CrowdinApi
     @Override
     public CrowdinApiException deserialize(JsonParser p, DeserializationContext ctxt) throws IOException {
         TreeNode treeNode = p.getCodec().readTree(p);
-        Iterable<String> iterable = treeNode::fieldNames;
-        List<String> fields = StreamSupport
-            .stream(iterable.spliterator(), false)
-            .collect(Collectors.toList());
-        if (fields.contains("errors")) {
-            return this.objectMapper.readValue(treeNode.toString(), HttpBadRequestException.class);
-        } else if (fields.contains("error")) {
-            return this.objectMapper.readValue(treeNode.toString(), HttpException.class);
+        TreeNode errors = treeNode.get("errors");
+
+        if (errors != null) {
+            TreeNode firstElement = errors.get(0);
+
+            if (firstElement != null && firstElement.get("index") != null) {
+                return this.objectMapper.treeToValue(treeNode, HttpBatchBadRequestException.class);
+            }
+
+            return this.objectMapper.treeToValue(treeNode, HttpBadRequestException.class);
+        } else if (treeNode.get("error") != null) {
+            return this.objectMapper.treeToValue(treeNode, HttpException.class);
         } else {
             return HttpException.fromMessage(treeNode.toString());
         }
