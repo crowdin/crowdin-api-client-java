b61bda5ea43cde4cfce81f58748321aa4f7ee6b0
dhfherna <dhfherna@bancolombia.com.co>
2024-10-31 16:22:06 -0500

    feature: Update gradle and lombok version

--- src/main/java/com/crowdin/client/notifications/model/SendNotificationToOrganizationMembersByUserIdsRequest.java
+++ src/main/java/com/crowdin/client/notifications/model/SendNotificationToOrganizationMembersByUserIdsRequest.java
@@ -1,10 +1,12 @@
 package com.crowdin.client.notifications.model;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import java.util.List;
 
 @Data
+@EqualsAndHashCode(callSuper = false)
 public class SendNotificationToOrganizationMembersByUserIdsRequest extends SendNotificationToOrganizationMembersRequest {
     private List<Long> userIds;
 }
