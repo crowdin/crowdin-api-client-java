018e8e3908e88738d61ab0b22c6ff5ac28d031f4
Yevheniy Oliynyk <evgen41293@gmail.com>
2025-11-11 16:45:06 +0100

    fix: qacheck enum values update

--- src/main/java/com/crowdin/client/translationstatus/model/Category.java
+++ src/main/java/com/crowdin/client/translationstatus/model/Category.java
@@ -4,8 +4,22 @@ import com.crowdin.client.core.model.EnumConverter;
 
 public enum Category implements EnumConverter<Category> {
 
-    EMPTY, VARIABLES, TAGS, PUNCTUATION, SYMBOL_REGISTER,
-    SPACES, SIZE, SPECIAL_SYMBOLS, WRONG_TRANSLATION, SPELLCHECK, ICU, DUPLICATE;
+    EMPTY,
+    VARIABLES,
+    TAGS,
+    PUNCTUATION,
+    SYMBOL_REGISTER,
+    SPACES,
+    SIZE,
+    SPECIAL_SYMBOLS,
+    WRONG_TRANSLATION,
+    SPELLCHECK,
+    ICU,
+    NUMBERS,
+    AI,
+    OUTDATED,
+    DUPLICATE,
+    TERMS;
 
     public static Category from(String value) {
         return Category.valueOf(value.toUpperCase());
