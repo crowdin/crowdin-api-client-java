ae1ae11d734c76a4df64f62e4acf8bed4d03f5e5
Danil Bubekov <bubekov.work@gmail.com>
2026-03-03 20:11:49 +0300

    git commit -m "chore: deprecate exportMode and fileIds (#361)"

--- src/main/java/com/crowdin/client/distributions/model/AddDistributionRequest.java
+++ src/main/java/com/crowdin/client/distributions/model/AddDistributionRequest.java
@@ -6,8 +6,14 @@ import java.util.List;
 
 @Data
 public class AddDistributionRequest {
+
+    @Deprecated
     private ExportMode exportMode;
+
     private String name;
+
+    @Deprecated
     private List<Long> fileIds;
+
     private List<Integer> bundleIds;
 }
